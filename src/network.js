9c9d4105cfa89e50a48493a2ebfd3d46825220a9
Bilal Bhatti <bilal@yobo.dev>
2021-10-23 22:29:28 +0500

    removed logo and added new one with coming soon

--- src/network.js
+++ src/network.js
@@ -1,46 +1,16 @@
 /* eslint-disable import/first */
 export const allNetworks = [
   {
-    name: 'BSC',
-    asset: 'BSC',
-    id: 56,
-    hash: '/bsc',
-  },
-  {
-    name: 'HECO',
-    asset: 'HECO',
-    id: 128,
-    hash: '/heco',
-  },
-  {
-    name: 'AVALANCHE',
-    asset: 'AVALANCHE',
-    id: 43114,
-    hash: '/avax',
-  },
-  {
     name: 'POLYGON',
     asset: 'POLYGON',
     id: 137,
     hash: '/polygon',
   },
   {
-    name: 'FANTOM',
-    asset: 'FANTOM',
-    id: 250,
-    hash: '/fantom',
-  },
-  {
-    name: 'HARMONY',
-    asset: 'HARMONY',
-    id: 1666600000,
-    hash: '/harmony',
-  },
-  {
-    name: 'ARBITRUM',
-    asset: 'ARBITRUM',
-    id: 42161,
-    hash: '/arbitrum',
+    name: 'ETHEREUM ( Coming Soon )',
+    asset: 'ETH',
+    id: 138,
+    hash: '/',
   },
 ];
 
